621b2bccb54c7559b21ed75908a2effcd677e79a
Nati <natalia@savvyy.ai>
2021-08-02 12:13:13 -0300

    perf: migration run to latest upgratind jest to v27

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/store',
     '<rootDir>/libs/shared-components',
     '<rootDir>/libs/assets',
