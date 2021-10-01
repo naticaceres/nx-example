acb51ad9253d565c62c311245cf032a4ae5c996b
Nati <natalia@savvyy.ai>
2021-10-01 17:45:35 -0300

    chore: cleanup

--- libs/shared-components/src/lib/shared-background/shared-background.component.ts
+++ libs/shared-components/src/lib/shared-background/shared-background.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bg-hoard-shared-background',
   templateUrl: './shared-background.component.html',
-  styleUrls: ['./shared-background.component.scss']
+  styleUrls: ['./shared-background.component.scss'],
 })
-export class SharedBackgroundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class SharedBackgroundComponent {}
